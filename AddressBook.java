4821bdcbe16b1a92b109b060447140f001819cbf
Divyagb29 <divya2229mgb@gmail.com>
2020-08-30 11:16:02 +0530

    taking user inputs to add into contacts

--- AddressBook.java
+++ AddressBook.java
@@ -3,7 +3,30 @@ package com.addressbook;
 import java.util.Scanner;
 
 public class AddressBook {
+    Scanner sc = new Scanner(System.in);
+    public void takeUserInputs()
+    {
+        System.out.println("---------------------------------");
+        System.out.println("Add contacts to the address book");
+        System.out.println("---------------------------------");
+        System.out.println("Enter First_Name : ");
+        String firstname = sc.next();
+        System.out.println("Enter Last_Name : ");
+        String lastname = sc.next();
+        System.out.println("Enter Address : ");
+        String address = sc.next();
+        System.out.println("Enter City : ");
+        String city = sc.next();
+        System.out.println("Enter State : ");
+        String state = sc.next();
+        System.out.println("Enter Pin_Code : ");
+        int pin = sc.nextInt();
+        System.out.println("Enter Phone_Number : ");
+        int phone = sc.nextInt();
+    }
     public static void main(String[] args) {
         System.out.println("*******WECLOME TO ADDRESS BOOK APPLICATION*******");
+        AddressBook ab = new AddressBook();
+        ab.takeUserInputs();
     }
 }
