95a64be1130264159ba52ba0f05f8977e0475c72
Divyagb29 <divya2229mgb@gmail.com>
2020-08-30 10:44:26 +0530

    Welcome Message

---
+++ AddressBook.java
@@ -0,0 +1,9 @@
+package com.addressbook;
+
+import java.util.Scanner;
+
+public class AddressBook {
+    public static void main(String[] args) {
+        System.out.println("*******WECLOME TO ADDRESS BOOK APPLICATION*******");
+    }
+}
